c105e390e55786d0978f650990ec4769606c0856
makis <mantzinos@hotmail.com>
2024-12-04 16:57:58 +0200

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit([FromRoute]int? id)
+        {
+            var category = new Category 
+            {
+                CategoryId = id.HasValue ? id.Value : 0
+
+            };
+            return View(category);
+        }
+    }
+}
